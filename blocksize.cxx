038a14a2ca61c8d3bc37c7aa40a3b7519ab1c5b8
WireFisher <20874936+WireFisher@users.noreply.github.com>
2019-01-21 19:36:33 +0800

    feat: add blocksize testcase

---
+++ blocksize.cxx
@@ -0,0 +1,86 @@
+/***************************************************************
+  *  Copyright (c) 2013, Tsinghua University.
+  *  This is a source file of C-Coupler.
+  *  This file was initially finished by Haoyu Yang,
+  *  supervised by Dr. Li Liu. If you have any problem,
+  *  please contact Dr. Li Liu via liuli-cess@tsinghua.edu.cn
+  ***************************************************************/
+
+#include <cstdio>
+#include <sys/time.h>
+#include <cstdlib>
+#include <cstring>
+#include "common_utils.h"
+#include "delaunay.h"
+
+char rand_fine[][64] = {
+    "lonlat_random_10000000_0.flat",
+    "lonlat_random_10000000_1.flat",
+};
+char rand_midd[][64] = {
+    "lonlat_random_1000000_0.flat",
+    "lonlat_random_1000000_1.flat",
+};
+char rand_cors[][64] = {
+    "lonlat_random_100000_0.flat",
+    "lonlat_random_100000_1.flat",
+};
+
+char path[] = "grid/expand/%s";
+
+int main(int argc, char* argv[])
+{
+    bool non_incremental = false;
+
+    Delaunay_Voronoi* triangulation = new Delaunay_Voronoi();
+
+    for (int i = 0; i < 2; i++)
+    {
+        char filepath[64];
+        snprintf(filepath, 64, path, cube_fine[i]);
+
+        if (i > 0 && non_incremental)
+            strncat(filepath, ".png.flat", 64);
+
+        FILE* fp = fopen(filepath, "r");
+        if (!fp) {
+            perror("No such file\n");
+            return -1;
+        }
+
+        int num_points = 0;
+        fscanf(fp, "%d", &num_points);
+        if (num_points < 1) {
+            perror("Wrong number of points\n");
+            return -1;
+        }
+
+        double* x = new double[num_points];
+        double* y = new double[num_points];
+
+        for (int i = 0; i < num_points; i++)
+            fscanf(fp, "%lf, %lf", &x[i], &y[i]);
+        fclose(fp);
+
+        if (non_incremental)
+        {
+            delete triangulation;
+            triangulation = new Delaunay_Voronoi();
+        }
+
+        timeval start, end;
+
+        gettimeofday(&start, NULL);
+        triangulation->add_points(x, y, num_points);
+        gettimeofday(&end, NULL);
+
+        triangulation->triangulate();
+
+        fprintf(stderr, "time: %ld us, points: %d\n", (end.tv_sec - start.tv_sec) * 1000000 + (end.tv_usec - start.tv_usec), num_points);
+
+        delete[] x;
+        delete[] y;
+    }
+
+    return 0;
+}
